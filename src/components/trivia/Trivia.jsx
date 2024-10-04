798e017b1dbea5ce8ec5bf3ebef9ff12f84ea958
juanmoncayo55 <juansebastianmoriones@unimayor.edu.co>
2024-10-04 17:49:44 -0500

    Agregando componente de quiz para que la trivia tenga su parte funcional.

--- src/components/trivia/Trivia.jsx
+++ src/components/trivia/Trivia.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link} from "react-router-dom";
-import HeadScore from "../utils/HeadScore.jsx"
+import Quiz from 'react-quiz-component';
+
+import HeadScore from "../utils/HeadScore.jsx";
+import {dataTrivia} from "./dataTrivia.js";
+import "./trivia.css";
 
 const Trivia = () => {
+	const [quizResult, setQuizResult] = useState({});
+
+	const quizResultFn = data => {
+		console.log(data)
+		setQuizResult(data);
+	}
+
 	return (
 		<>
 			<HeadScore title="RESPONDE LA TRIVIA ACORDE A LA INFORMACIÓN" />
-			<div className="conatiner mx-auto px-5 flex flex-col gap-8">
-				<p className="text-azulBrillante3 text-center text-xl mt-5">con base en lo aprendido en Tángara</p>
-
-				<div className="bg-verdeLimon rounded-2xl py-4">
-					<p className="text-black uppercase text-lg font-semibold text-center">Gira la Ruleta <br /> y Acepta el Reto</p>
-				</div>
-
-				<div className="flex flex-col gap-3">
-					{
-						[1,2,3].map((item) => (
-							<Link key={item} to={item == 1 ? "/hangman" : null} className="bg-white rounded-3xl text-backgroundAzulOscuro uppercase text-xl text-center font-bold py-3">Respuesta {item}</Link>
-						))
-					}
-				</div>
+			<div className="conatiner mx-auto px-5 flex flex-col">
+				{ quizResult.numberOfCorrectAnswers !== undefined ? "" : <p className="text-azulBrillante3 text-center text-xl mt-5">con base en lo aprendido en Tángara</p>}
+				<Quiz
+					quiz={dataTrivia}
+					disableSynopsis
+					enableProgressBar={false}
+					showDefaultResult={false}
+					onComplete={quizResultFn}
+				/>
 			</div>
+			{ quizResult.numberOfCorrectAnswers && (
+				<>
+					<div className="flex flex-col justify-center gap-y-2">
+						<p className="text-white text-lg text-center">Preguntas Correctas: {quizResult.numberOfCorrectAnswers}</p>
+						<p className="text-white text-lg text-center">Preguntas Incorrectas: {quizResult.numberOfIncorrectAnswers}</p>
+						<p className="text-white text-lg text-center">Total Puntos: {quizResult.correctPoints} / {quizResult.totalPoints} </p>
+						<Link to="/hangman" className="bg-azulBrillante3 text-black text-lg font-semibold py-3 px-9 rounded-3xl w-1/2 mx-auto text-center mt-7">Siguiente</Link>
+					</div>
+				</>
+			) }
 		</>
 	)
 }
