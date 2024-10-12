048556ec8e0e1842a377542f6df626b84ffea023
pablo_alejandro <pablo_alejandro@bitbucket.org>
2024-10-12 00:42:10 -0500

    responsive sliding-puzzle with customizable grid, steps counter, has won message & start-reset buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import React from "react";
 import {Routes, Route} from "react-router-dom";
 import Roulette from "./components/roulette/Roulette";
 import Trivia from "./components/trivia/Trivia.jsx";
 import Hangman from "./components/hangman/Hangman.jsx";
 import Area from "./components/area/Area.jsx";
+import Puzzle from "./components/puzzle/Puzzle.jsx";
 
 function App() {
 
@@ -15,6 +15,7 @@ function App() {
           <Route path="/trivia" element={<Trivia />} />
           <Route path="/hangman" element={<Hangman />} />
           <Route path="/area" element={<Area />} />
+          <Route path="/puzzle" element={<Puzzle numeroColumnas={3} urlImagen={'https://placedog.net/800/800?r'} />} />
         </Routes>
       </div>
     </>
